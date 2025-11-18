08b964e8bf78b3b42ff35537c91b6e9916bb67f2
VIVOBOOK\iulia <iulian.arg@outlook.com>
2025-11-18 11:08:18 +0200

    WebServer: Add AsyncWebServerManager for web interface with sliders

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,8 @@
 #include "ConfigManager.h"
 #include "WifiManager.h"
 #include "TimeManager.h"
-// #include "IOManager.h"
+#include "AsyncWebServerManager.h"
 #include "PositionManager.h"
-// #include "ButtonLEDManager.h"
-// #include "BtnLEDManager.h"
-// #include "RelayManager.h"
 
 BoardPowerManager *boardPowerManager;
 WifiManager *wifiManager;
@@ -30,6 +27,7 @@ SensorManager *sensorManager;
 ConfigManager *configManager;
 TimeManager *timeManager;
 PositionManager *positioningManager;
+AsyncWebServerManager *asyncWebServerManager;
 // RelayManager *relayManager;
 // ButtonLEDManager *buttonLEDManager;
 // BtnLEDManager *btnLEDManager;
@@ -48,15 +46,11 @@ void setup()
     boardPowerManager = new BoardPowerManager();
     boardPowerManager->InitBoardPowerManager();
 
-    // bleManager = new NimBLEManager();
-    // bleManager->setupNimBLE(currentProgram);
-    // lastProgramTimestamp = millis();
-
     configManager = new ConfigManager();
     config = configManager->readConfig();
 
     wifiManager = new WifiManager();
-    // wifiManager->WifiConnect(config);
+    wifiManager->WifiConnect(config);
 
     sensorManager = new SensorManager();
     sensorManager->SetupSensors();
@@ -64,19 +58,13 @@ void setup()
     timeManager = new TimeManager();
     timeManager->initTime(config);
 
-    // relayManager = new RelayManager(config);
-    // relayManager->ResetRelays();
-    // ioManager = new IOManager(config);
-    // ioManager->ResetRelays();
-
     positioningManager = new PositionManager(config, sensorManager);
-    // ioManager->SetPositionManager(positioningManager);
 
-    // buttonLEDManager = new ButtonLEDManager(config, positioningManager);
-    // relayManager->SetButtonLEDManager(buttonLEDManager);
+    asyncWebServerManager = new AsyncWebServerManager();
+    asyncWebServerManager->initWebServer();
 
     myTicker.attach(1.0, tick);
-    config.POT1_pin_MaxAngl = 35;
+    // config.POT1_pin_MaxAngl = 35;
 }
 unsigned long previousPositioningMillis = 0;
 unsigned long previousButtonMillis = 0;
@@ -108,5 +96,6 @@ void loop()
         previousButtonMillis = millis();
         positioningManager->MonitorBtnStates();
         positioningManager->UpdateLEDStates();
+        asyncWebServerManager->loopWebServer();
     }
 }
